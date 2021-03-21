bc5ebe759a4bd301e226199696eeb4c738238bac
Onur Akalın <onurakalin96@gmail.com>
2021-03-22 01:10:17 +0300

    Gereksiz dosyalar temizlendi

--- TabloAnalizi/Colon.cs
+++
@@ -1,8 +0,0 @@
-namespace TabloAnalizi
-{
-    public class Colon
-    {
-        public int Id { get; set; }
-        public string TableName { get; set; }
-    }
-}
--- TabloAnalizi/Ortak.cs
+++
@@ -1,9 +0,0 @@
-namespace TabloAnalizi
-{
-    public class Ortak
-    {
-        public int Id { get; set; }
-        public string TableName { get; set; }
-        public bool HasSiteId { get; set; }
-    }
-}
